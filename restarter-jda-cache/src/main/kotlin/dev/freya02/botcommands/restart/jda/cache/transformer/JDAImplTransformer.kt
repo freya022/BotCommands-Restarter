8962737f26f280e0097d0d57c8acc489f9b986c1
freya02 <41875020+freya022@users.noreply.github.com>
2025-06-06 20:25:04 +0200

    JDA cache: Remove note

--- restarter-jda-cache/src/main/kotlin/dev/freya02/botcommands/restart/jda/cache/transformer/JDAImplTransformer.kt
+++ restarter-jda-cache/src/main/kotlin/dev/freya02/botcommands/restart/jda/cache/transformer/JDAImplTransformer.kt
@@ -19,7 +19,6 @@ private val CD_JDABuilderSession = classDesc<JDABuilderSession>()
 
 private const val cacheKeyFieldName = "cacheKey"
 
-// TODO add transform on JDAImpl#login(String, ShardInfo, Compression, boolean, int, GatewayEncoding)
 internal object JDAImplTransformer : AbstractClassFileTransformer("net/dv8tion/jda/internal/JDAImpl") {
 
     override fun transform(classData: ByteArray): ByteArray {
